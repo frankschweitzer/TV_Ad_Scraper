d7fe82e193757b592eda381e0b3a513f317210ef
frankschweitzer <schwfj20@wfu.edu>
2023-05-15 23:13:29 -0400

    final commit

--- tvScraper.py
+++ tvScraper.py
@@ -14,16 +14,28 @@ import pandas as pd
 
 def main():
     networks = ["A&E", "AMC", "ANML", "BBCA", "BET", "BHER", "BRVO", "CMT", "E!", "FX", "FXM", "FYI", "GOLF", "HGTV", "HIST", "ID", "IFC", "LMN", "MLB", "NGC", "OWN", "PAR", "POP", "SYFY", "TLC", "TNT", "TRVL", "USA", "VH1", "VICE"]
+    # reading the dates and times needed
+    name = "SampleRedBullData.xlsx"
+    data = read_file(name)
+    desired_map = {}
     
-    desired_time = "12:15 PM"
+    for i in range(len(data)):
+        curr_network_list = desired_map.get(data[i][2], [])
+        curr_data = []
+        curr_data = [data[i][0], data[i][1]]
+        curr_network_list.append(curr_data)
+        desired_map[data[i][2]] = curr_network_list
     
     network_to_map = {}
-    # need to make a loop, then this returns the map for each network and we can create another map from networks to this map
-    map, times_each_day, dates = show_data(networks[0]) # returns map from dates to map of shwos and times
-    network_to_map.update({networks[0]: map})
-    # print(network_to_map.get("A&E").get("Sunday, May 14").get("12:03 AM"))
+    network_to_times = {}
+    for network in networks:
+        # need to make a loop, then this returns the map for each network and we can create another map from networks to this map
+        if network == "A&E" or network == "FX":
+            map, times_each_day, dates = show_data(network) # returns map from dates to map of shows and times
+            network_to_times.update({network: times_each_day})
+            network_to_map.update({network: map})
     
-    # cleaning up dates
+    # cleaning up dates to use for indices
     start_day = dates[0]
     end_day = dates[-1]
     start_date = parser.parse(start_day).date()
@@ -31,18 +43,27 @@ def main():
     date_length = (end_date - start_date).days
     dates = list_dates(start_date, date_length) # list of dates from start to end
     
-    count = 0
-    # printing the show at the desired time for all of the dates
-    for date in dates:
-        # print(date + ' ' + locate_show(times_each_day, desired_time, date, count, map))
-        count+=1
-    
-    # reading the dates and times needed
-    name = "SampleRedBullData.xlsx"
-    data = read_file(name)
-    print(data[0])
+    # get the shows I want
+    results = [[]]
+    for network, shows in network_to_map.items():
+        times = network_to_times.get(network) # times list
+        desired_list = desired_map.get(network) # day will be at first position, time wanted at second
+        for curr in desired_list:
+            curr_date = parser.parse(curr[0]).date()
+            ind = (curr_date - start_date).days
+            show_title = locate_show(times, curr[1], curr[0], ind, shows)
+            current = []
+            date_object = datetime.strptime(curr[0], "%A, %B %d")
+            formatted_date = date_object.strftime("%m/%d/%Y")
+            current.append(formatted_date)
+            current.append(curr[1])
+            current.append(network)
+            current.append(show_title)
+            results.append(current)
+
     # write all the shows and times to file
-    # write_to_file(map, network)
+    results.pop(0)
+    write_to_file(results)
     
 
 # extracting data needed from excel sheet
@@ -55,14 +76,18 @@ def read_file(name):
     data = []
     for i in range(len(dates)):
         curr_list = []
-        curr_list.append(dates[i]) # match the dates to mine
-        curr_list.append(times[i][0:4]+times[i][-3:])
+        tmp_date = datetime.strptime(dates[i], "%m/%d/%Y")
+        date_formatted = tmp_date.strftime("%A, %B %d")
+        curr_list.append(date_formatted)
+        tmp_time = times[i].split(" ")
+        time = tmp_time[0][0:-3]+' '+tmp_time[1]
+        curr_list.append(time)
         curr_list.append(networks[i])
         data.append(curr_list)
         
     return data
     
-    
+# returns a list of the dates that the html sheets cover
 def list_dates(start, size):
     dates = []
     for i in range(size+1):
@@ -71,7 +96,8 @@ def list_dates(start, size):
         dates.append(target_day)
     return dates
 
-    
+
+# returns the show that will be playing at the given time
 def locate_show(times, time_wanted, day, index, map):
     curr_map = map.get(day)
     curr_times = times[index]
@@ -89,35 +115,38 @@ def locate_show(times, time_wanted, day, index, map):
             break
     return curr_map.get(time_index)
     
-
-def write_to_file(map, network):
+# writes our inserted data into the file
+def write_to_file(results):
     # print to an excel file
     workbook = openpyxl.Workbook()
     bold_font = Font(bold=True)
     sheet = workbook.active
-    sheet["A1"] = "Date"
+    sheet["A1"] = "Advertiser"
     cell_A1 = sheet["A1"]
     cell_A1.font = bold_font
-    sheet["B1"] = "Time"
+    sheet["B1"] = "Date"
     cell_B1 = sheet["B1"]
     cell_B1.font = bold_font
-    sheet["C1"] = "Network"
+    sheet["C1"] = "Time"
     cell_C1 = sheet["C1"]
     cell_C1.font = bold_font
-    sheet["D1"] = "Show"
+    sheet["D1"] = "Network"
     cell_D1 = sheet["D1"]
     cell_D1.font = bold_font
+    sheet["E1"] = "Program"
+    cell_E1 = sheet["E1"]
+    cell_E1.font = bold_font
     row_num = 2
-    for key, value in map.items():
-        for subkey, subval in value.items():
-            sheet[f"A{row_num}"] = key # insert day
-            sheet[f"B{row_num}"] = subkey # insert time
-            sheet[f"C{row_num}"] = network # insert day
-            sheet[f"D{row_num}"] = subval # insert show
-            row_num += 1
+    for result in results:
+        sheet[f"A{row_num}"] = "Red Bull"
+        sheet[f"B{row_num}"] = result[0] # insert day
+        sheet[f"C{row_num}"] = result[1] # insert time
+        sheet[f"D{row_num}"] = result[2] # insert day
+        sheet[f"E{row_num}"] = result[3] # insert show
+        row_num += 1
     workbook.save("tvData.xlsx")
 
-
+# returns a map of all of the data given by day
 def show_data(network):
     # open page via downloaded html file
     with open(f'{network}.html') as fp:
@@ -155,7 +184,8 @@ def show_data(network):
     shows_by_day.pop(0) # remove null list
     map = {}
     i = 0
-    # grouping the shows and times by day
+    # grouping the shows and times by day - but here is where I need to only add the ones needed based on the
+    # search and clean parsing I did from reading the excel file
     for curr_shows in shows_by_day:
         curr_times = times_by_day[i]
         curr_map = {}
